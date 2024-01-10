49e76907cf0a2c3797031baa91f78a9e31253f48
iyasu2 <iyasuforreal@gmail.com>
2024-01-10 21:06:13 +0300

    s

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,16 +1,9 @@
-export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) {
-    throw new Error('Cannot process');
-  }
+const updateUniqueItems = (map) => {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
 
-  const updatedMap = new Map();
-  for (const [key, value] of map) {
-    if (value === 1) {
-      updatedMap.set(key, 100);
-    } else {
-      updatedMap.set(key, value);
-    }
-  }
+  map.forEach((value, key) => {
+    if (value === 1) map.set(key, 100);
+  });
+};
 
-  return updatedMap;
-}
+export default updateUniqueItems;
