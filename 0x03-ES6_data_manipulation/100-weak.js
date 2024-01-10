ba152ec677e5289f407f1c8ea4599c586d302ef6
iyasu2 <iyasuforreal@gmail.com>
2024-01-10 20:54:17 +0300

    s

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,16 @@
+const weakMap = new WeakMap();
+
+export default function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+
+  const count = weakMap.get(endpoint) + 1;
+  weakMap.set(endpoint, count);
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  // Your API query logic here
+}
