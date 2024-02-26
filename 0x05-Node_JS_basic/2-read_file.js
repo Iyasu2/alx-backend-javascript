61fa84f40c0952cc3eddd46c5dfba5370c105ebb
iyasu2 <iyasuforreal@gmail.com>
2024-02-26 18:56:22 +0300

    s

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+/**
+ * This function counts students in a database.
+ * @param {string} path - path to file.
+ * @author Iyasu Asnake <https://github.com/Iyasu2>
+ */
+function countStudents(path) {
+  let data;
+
+  try {
+    data = fs.readFileSync(path, 'utf8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  let lines = data.split('\n');
+  lines = lines.filter((line) => line.length > 0 && !line.includes('firstname')); // Remove empty lines and header
+
+  console.log(`Number of students: ${lines.length}`);
+
+  const fields = {};
+  lines.forEach((line) => {
+    const student = line.split(',');
+    const field = student[3];
+    if (!fields[field]) {
+      fields[field] = [];
+    }
+    fields[field].push(student[0]);
+  });
+
+  for (const field in fields) {
+    console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+  }
+}
+
+module.exports = countStudents;
