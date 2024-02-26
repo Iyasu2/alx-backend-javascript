060e98e1d8c5782c36fa8160dcd131bd3a7ff8f8
iyasu2 <iyasuforreal@gmail.com>
2024-02-26 18:29:19 +0300

    s

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,7 @@
 /**
  * This function prints a message to the console.
  * @param {string} message - The message to print.
+ * @author Iyasu Asnake <https://github.com/Iyasu2>
  */
 const displayMessage = (message) => {
     console.log(message);
