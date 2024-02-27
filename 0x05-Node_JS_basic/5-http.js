47ff1d7b0147e788c2951ac46ebaaba98bcfd03e
iyasu2 <iyasuforreal@gmail.com>
2024-02-27 18:15:00 +0300

    s

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,29 +1,68 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
+/**
+ * Read the database file and store student data.
+ * @param {String} databasePath The path to the CSV data file.
+ * @throws {Error} Throws an error if the database cannot be loaded.
+ * @returns {Array} An array of student data (filtered and cleaned).
+ * @author Iyasu Asnake <https://github.com/Iyasu2>
+ */
+function readDatabaseFile(databasePath) {
+  try {
+    const data = fs.readFileSync(databasePath, 'utf8');
+    const lines = data.split('\n');
+    return lines.filter((line) => line.trim().length > 0 && !line.includes('firstname'));
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+/**
+ * Create an HTTP server.
+ * @param {String} databasePath The path to the CSV data file.
+ * @returns {http.Server} The HTTP server instance.
+ */
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
-    res.end('Hello Holberton School!');
+    // Handle root path
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!\n');
   } else if (req.url === '/students') {
-    countStudents(process.argv[2])
-      .then((data) => {
-        res.write('This is the list of our students\n');
-        res.write(`Number of students: ${data.students.length}\n`);
-        Object.entries(data.byField).forEach(([field, students]) => {
-          res.write(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}\n`);
-        });
-        res.end();
-      })
-      .catch((error) => {
-        res.end(error.message);
-      });
-  } else {
+    // Handle /students path
+    const databasePath = process.argv[2]; // Get the database file path from command line arguments
+    const studentData = readDatabaseFile(databasePath);
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n');
+    res.write(`Number of students: ${studentData.length}\n`);
+
+    const fields = {};
+    studentData.forEach((line) => {
+      const student = line.split(',');
+      const field = student[3];
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(student[0]);
+    });
+
+    for (const field in fields) {
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        res.write(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`);
+      }
+    }
     res.end();
+  } else {
+    // Handle other paths
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found\n');
   }
 });
 
-app.listen(1245);
+// Listen on port 1245
+app.listen(1245, () => {
+  console.log('Server listening on port 1245');
+});
 
 module.exports = app;
