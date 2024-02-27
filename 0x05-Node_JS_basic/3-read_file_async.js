6dad745af3bbf2d8bc1d43f13c3cd28ffa6a464f
iyasu2 <iyasuforreal@gmail.com>
2024-02-27 18:02:56 +0300

    s

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -5,31 +5,32 @@ const fs = require('fs').promises;
  * @param {String} dataPath The path to the CSV data file.
  * @author Iyasu Asnake <https://github.com/Iyasu2>
  */
-function countStudents(path) {
-  return fs.readFile(path, 'utf8')
-    .then((data) => {
-      let lines = data.split('\n');
-      lines = lines.filter((line) => line.length > 0 && !line.includes('firstname')); // Remove empty lines and header
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    let lines = data.split('\n');
+    lines = lines.filter((line) => line.length > 0 && !line.includes('firstname')); // Remove empty lines and header
 
-      console.log(`Number of students: ${lines.length}`);
+    console.log(`Number of students: ${lines.length}`);
 
-      const fields = {};
-      lines.forEach((line) => {
-        const student = line.split(',');
-        const field = student[3];
-        if (!fields[field]) {
-          fields[field] = [];
-        }
-        fields[field].push(student[0]);
-      });
+    const fields = {};
+    for (const line of lines) {
+      const student = line.split(',');
+      const field = student[3];
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(student[0]);
+    }
 
-      for (const field in fields) {
+    for (const field in fields) {
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
         console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
       }
-    })
-    .catch(() => {
-      throw new Error('Cannot load the database');
-    });
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
