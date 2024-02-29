4bd51664f95452c7f5417bdc15e5115859d316bd
iyasu2 <iyasuforreal@gmail.com>
2024-02-29 20:27:04 +0300

    s

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,18 @@
+const { expect } = require('chai');
+const request = require('request');
+
+describe('Index page', () => {
+  it('returns correct status code', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('returns correct result', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
