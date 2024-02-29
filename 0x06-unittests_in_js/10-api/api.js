20dc726ec445f83d3b82698cb83ba279eecef6f1
iyasu2 <iyasuforreal@gmail.com>
2024-02-29 20:49:58 +0300

    s

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,28 +1,36 @@
 const express = require('express');
-const app = express();
-const port = 7865;
 
-// Define a route for GET /
-app.get('/', (req, res) => {
+const app = express();
+const PORT = 7865;
+
+app.use(express.json());
+
+app.get('/', (_req, res) => {
   res.send('Welcome to the payment system');
 });
 
-// Define a route for GET /available_payments
-app.get('/available_payments', (req, res) => {
-  const paymentMethods = {
-    credit_cards: true,
-    paypal: false,
-  };
-  res.json({ payment_methods: paymentMethods });
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
 });
 
-// Define a route for POST /login
+app.get('/available_payments', (_req, res) => {
+  res.json({ payment_methods: { credit_cards: true, paypal: false } });
+});
+
 app.post('/login', (req, res) => {
-  const { userName } = req.body;
-  res.send(`Welcome ${userName}`);
+  let username = '';
+
+  if (req.body) {
+    username = req.body.userName;
+  }
+
+  res.send(`Welcome ${username}`);
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
+
+module.exports = app;
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,33 +1,53 @@
-const { expect } = require('chai');
 const request = require('request');
+const { expect } = require('chai');
 
-describe('Available payments endpoint', () => {
-  it('returns correct payment methods', (done) => {
-    request('http://localhost:7865/available_payments', (error, response, body) => {
-      const expectedResponse = {
-        payment_methods: {
-          credit_cards: true,
-          paypal: false,
-        },
-      };
-      expect(JSON.parse(body)).to.deep.equal(expectedResponse);
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
+
+  it('/ returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
   });
-});
 
-describe('Login endpoint', () => {
-  it('returns correct welcome message', (done) => {
-    const userName = 'Betty';
-    request.post(
-      {
-        url: 'http://localhost:7865/login',
-        json: { userName },
-      },
-      (error, response, body) => {
-        expect(body).to.equal(`Welcome ${userName}`);
-        done();
-      }
-    );
+  it('/cart/:id returns correct response for valid id', (done) => {
+    request.get(`${API_URL}/cart/47`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 47');
+      done();
+    });
+  });
+
+  it('/cart/:id returns 404 response for negative numbera in id', (done) => {
+    request.get(`${API_URL}/cart/-47`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('/cart/:id returns 404 response for non-numeric values in id', (done) => {
+    request.get(`${API_URL}/cart/d200-44a5-9de6`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('/login returns valid response', (done) => {
+    request.post(`${API_URL}/login`, {json: {userName: 'Pinkbrook'}}, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome Pinkbrook');
+      done();
+    });
+  });
+
+  it('/available_payments returns valid response', (done) => {
+    request.get(`${API_URL}/available_payments`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(JSON.parse(body))
+        .to.be.deep.equal({payment_methods: {credit_cards: true, paypal: false}});
+      done();
+    });
   });
 });
