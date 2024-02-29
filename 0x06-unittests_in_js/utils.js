aa8ef0447a69c4f5d9b912f9238443b64325ed5a
iyasu2 <iyasuforreal@gmail.com>
2024-02-29 19:47:49 +0300

    s

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,31 @@
+// utils.js
+
+/**
+ * Performs various calculations based on the provided type.
+ * @param {string} type - Type of calculation (SUM, SUBTRACT, or DIVIDE).
+ * @param {number} a - First number.
+ * @param {number} b - Second number.
+ * @returns {number|string} - Result of the specified calculation or 'Error'.
+ */
+function calculateNumber(type, a, b) {
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
+
+  switch (type) {
+    case 'SUM':
+      return roundedA + roundedB;
+    case 'SUBTRACT':
+      return roundedA - roundedB;
+    case 'DIVIDE':
+      if (roundedB === 0) {
+        return 'Error';
+      }
+      return roundedA / roundedB;
+    default:
+      return 'Invalid type';
+  }
+}
+
+module.exports = {
+  calculateNumber,
+};
